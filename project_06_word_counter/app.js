bd740e9d748bf996b8fd3f2a45bb46c12e29b70c
HamzaKhursheed <hamzakhursheed2007@gmail.com>
2023-09-10 00:03:00 +0500

    Added project 5 & 6

---
+++ project_06_word_counter/app.js
@@ -0,0 +1,5 @@
+import cliInput from "../utils/cliInputs.js";
+const inputData = await cliInput("input", "str", "Enter an english paragraph.");
+// const data1 = '"Please select in which currency you want to convert"';
+const data = inputData.str;
+console.log(data.split("").filter((el) => el != " ").length);
